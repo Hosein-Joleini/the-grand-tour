0ac20d48856b9296e4d9a78b3b96bdaf010dd194
Hosein-Joleini <hosseinjoleini94@gmail.com>
2024-07-14 20:20:23 +0330

    Project is prepared for deployment

--- src/ui/ControlledDatePicker.tsx
+++ src/ui/ControlledDatePicker.tsx
@@ -5,7 +5,6 @@ import DatePicker from 'react-multi-date-picker';
 
 import persian_fa from 'react-date-object/locales/persian_fa';
 import persian from 'react-date-object/calendars/persian';
-import transition from 'react-element-popper/animations/transition';
 
 const datePickerInputClasses =
   'outline-none rounded-md border px-1.5 py-2 border-gray-300 bg-emerald-50 focus:ring-2 focus:ring-emerald-600 focus:border-emerald-600 text-base text-slate-700 hover:bg-emerald-100 transition-all duration-300 w-full dark:bg-gray-900 dark:hover:bg-gray-800';
@@ -35,7 +34,6 @@ export default function ControlledDatePicker({
             minDate={new Date()}
             id={id}
             inputClass={datePickerInputClasses}
-            animations={[transition({ duration: 500, from: 35 })]}
             locale={persian_fa}
             calendar={persian}
             onChange={(date): void => {
